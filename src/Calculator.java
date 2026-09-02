68be0722edc7f435a3bbe07e7de2477624bbe5c2
Beibarys Kanat <bisb25-12@NTDOM.LOCAL>
2026-09-02 09:29:39 +0500

    Add Calculator.java

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    public static void main(String[] args){
+        String name="apple";
+        char category='a';
+        boolean inStock=true;
+        int amount=50;
+        double price=1111.11;
+
+        System.out.println(
+                "Product: " + name +
+                ", Quantity: " + amount +
+                ", Price: " + price +
+                ", In stock: " + inStock +
+                ", Category: " + category
+
+        );
+    }
+}
